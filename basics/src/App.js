5be937d044183774b86db561af9fc5091331e859
Alvin Galit <aroblesgalit@gmail.com>
2022-06-08 22:46:24 -0600

    Created new directory for a simple todo app practice

--- basics/src/App.js
+++ basics/src/App.js
@@ -1,14 +1,25 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import TodoList from './components/TodoList';
+import uuidv4 from 'uuid/v4';
 
 function App() {
   const [todos, setTodos] = useState([]);
+  const todoNameRef = useRef();
+
+  function handleAddTodo(e) {
+    const name = todoNameRef.current.value;
+    if (name === '') return;
+    setTodos(prevTodos => {
+      return [...prevTodos, { id: uuidv4, name: name, complete: false }]
+    })
+    todoNameRef.current.value = null;
+  }
 
   return (
     <>
       <TodoList todos={todos} />
-      <input type='text' />
-      <button>Add Todo</button>
+      <input ref={todoNameRef} type='text' />
+      <button onClick={handleAddTodo}>Add Todo</button>
       <button>Clear Completed Todos</button>
       <div>0 left to do</div>
     </>
