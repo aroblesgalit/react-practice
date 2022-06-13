959676628084f1c84866f42b2397aefc58a62113
Alvin Galit <aroblesgalit@gmail.com>
2022-06-12 22:00:09 -0600

    Created state for inputText

--- todo_app/client/src/App.js
+++ todo_app/client/src/App.js
@@ -1,10 +1,14 @@
+import React, { useState } from 'react';
 import './App.css';
 import Form from './components/Form';
 import TodoList from './components/TodoList';
 
 function App() {
+
+  const [inputText, setInputText] = useState('');
+
   return (
-    <div className="App">
+    <div className='App'>
       <header>
         <h1>My Todo List</h1>
       </header>
