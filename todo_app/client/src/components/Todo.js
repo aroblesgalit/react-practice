3daa8b5fd13dbb981b3157f20613ab465e043ae1
Alvin Galit <aroblesgalit@gmail.com>
2022-06-14 23:34:06 -0600

    Created template in Todo

--- todo_app/client/src/components/Todo.js
+++ todo_app/client/src/components/Todo.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 export default function Todo() {
   return (
-    <div>Todo</div>
+    <div className='todo'>
+        <li className='todo-item'>Heyy</li>
+        <button className='complete-btn'><i className='fas fa-check'></i></button>
+        <button className='trash-btn'><i className='fas fa-trash'></i></button>
+    </div>
   );
 }
