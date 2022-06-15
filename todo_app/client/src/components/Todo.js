9f2e092bc1fd3cb525c981bc18ff9ac16f9911c7
Alvin Galit <aroblesgalit@gmail.com>
2022-06-14 23:47:22 -0600

    Passed down todos state to todo component

--- todo_app/client/src/components/Todo.js
+++ todo_app/client/src/components/Todo.js
@@ -1,9 +1,9 @@
 import React from 'react';
 
-export default function Todo() {
+export default function Todo({ todo }) {
   return (
     <div className='todo'>
-        <li className='todo-item'>Heyy</li>
+        <li className='todo-item'>{todo.text}</li>
         <button className='complete-btn'><i className='fas fa-check'></i></button>
         <button className='trash-btn'><i className='fas fa-trash'></i></button>
     </div>
--- todo_app/client/src/components/TodoList.js
+++ todo_app/client/src/components/TodoList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Todo from './Todo'
 
-export default function TodoList() {
+export default function TodoList({ todos }) {
   return (
     <div className='todo-container'>
         <ul className='todo-list'>
-          <Todo />
+          {
+            todos.map(todo => <Todo key={todo.id} todo={todo} />)
+          }
         </ul>
     </div>
   )
