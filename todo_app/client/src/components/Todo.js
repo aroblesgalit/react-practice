a5a25c2fc19f653889c223190c45643ee67a01df
Alvin Galit <aroblesgalit@gmail.com>
2022-06-15 19:35:23 -0600

    Created handler for checking off an item

--- todo_app/client/src/components/Todo.js
+++ todo_app/client/src/components/Todo.js
@@ -5,10 +5,17 @@ export default function Todo({ todo, todos, setTodos }) {
     setTodos(todos.filter(el => el.id !== todo.id));
   }
 
+  function completeHandler() {
+    const tempTodos = [...todos];
+    const index = tempTodos.findIndex(el => el.id === todo.id);
+    tempTodos[index]['completed'] = !tempTodos[index]['completed'];
+    setTodos(tempTodos);
+  }
+
   return (
-    <div className='todo'>
+    <div className={`todo ${todo.completed && 'completed'}`}>
         <li className='todo-item'>{todo.text}</li>
-        <button className='complete-btn'><i className='fas fa-check'></i></button>
+        <button onClick={completeHandler} className='complete-btn'><i className='fas fa-check'></i></button>
         <button onClick={deleteHandler} className='trash-btn'><i className='fas fa-trash'></i></button>
     </div>
   );
