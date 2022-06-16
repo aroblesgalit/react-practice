7f080ca23dc6dbba7086377d96f0b25fe203c395
Alvin Galit <aroblesgalit@gmail.com>
2022-06-15 19:21:37 -0600

    Created handler for deleting an item

--- todo_app/client/src/components/Todo.js
+++ todo_app/client/src/components/Todo.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
-export default function Todo({ todo }) {
+export default function Todo({ todo, todos, setTodos }) {
+  function deleteHandler() {
+    setTodos(todos.filter(el => el.id !== todo.id));
+  }
+
   return (
     <div className='todo'>
         <li className='todo-item'>{todo.text}</li>
         <button className='complete-btn'><i className='fas fa-check'></i></button>
-        <button className='trash-btn'><i className='fas fa-trash'></i></button>
+        <button onClick={deleteHandler} className='trash-btn'><i className='fas fa-trash'></i></button>
     </div>
   );
 }
--- todo_app/client/src/components/TodoList.js
+++ todo_app/client/src/components/TodoList.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import Todo from './Todo'
 
-export default function TodoList({ todos }) {
+export default function TodoList({ todos, setTodos }) {
   return (
     <div className='todo-container'>
         <ul className='todo-list'>
           {
-            todos.map(todo => <Todo key={todo.id} todo={todo} />)
+            todos.map(todo => <Todo key={todo.id} todo={todo} todos={todos} setTodos={setTodos} />)
           }
         </ul>
     </div>
