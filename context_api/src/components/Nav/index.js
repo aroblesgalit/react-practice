b73b6181230a84e8ae486300ac525726e9c8d162
Alvin Galit <aroblesgalit@gmail.com>
2022-06-19 00:30:45 -0600

    Used movie context api in nav to render movie count

--- context_api/src/components/Nav/index.js
+++ context_api/src/components/Nav/index.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import MovieContext from '../../utils/MovieContext';
 
 export default function Nav() {
+
+    const { movies } = useContext(MovieContext);
+
   return (
     <div>
         <h3>Alvin Galit</h3>
-        <p>List of Movies: </p>
+        <p>List of Movies: {movies.length}</p>
     </div>
   );
 }
