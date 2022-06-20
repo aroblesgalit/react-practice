fc6f01519783ea00d3ea0b588c24db90a7159ed4
Alvin Galit <aroblesgalit@gmail.com>
2022-06-19 19:09:01 -0600

    Added on change events to input fields to set name and price

--- context_api/src/components/AddMovie/index.js
+++ context_api/src/components/AddMovie/index.js
@@ -5,11 +5,11 @@ export default function AddMovie() {
     const [name, setName] = useState('');
     const [price, setPrice] = useState(0);
 
-  return (
-    <form>
-        <input type='text' name='name' />
-        <input type='number' name='price' />
-        <button type='submit'>Submit</button>
-    </form>
-  );
+    return (
+        <form>
+            <input type='text' name='name' onChange={e => setName(e.target.value)}/>
+            <input type='number' name='price' onChange={e => setPrice(`$${parseFloat(e.target.value).toFixed(2)}`)} />
+            <button type='submit'>Submit</button>
+        </form>
+    );
 }
