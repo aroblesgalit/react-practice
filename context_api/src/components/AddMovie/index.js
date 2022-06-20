04294660c9d76504422b61995b8b31837d169a07
Alvin Galit <aroblesgalit@gmail.com>
2022-06-19 19:10:34 -0600

    Created function for adding a movie

--- context_api/src/components/AddMovie/index.js
+++ context_api/src/components/AddMovie/index.js
@@ -5,10 +5,15 @@ export default function AddMovie() {
     const [name, setName] = useState('');
     const [price, setPrice] = useState(0);
 
+    function addMovie(e) {
+        e.preventDefault();
+        console.log(name, price);
+    }
+
     return (
         <form>
-            <input type='text' name='name' onChange={e => setName(e.target.value)}/>
-            <input type='number' name='price' onChange={e => setPrice(`$${parseFloat(e.target.value).toFixed(2)}`)} />
+            <input type='text' name='name' value={name} onChange={e => setName(e.target.value)}/>
+            <input type='number' name='price' value={price} onChange={e => setPrice(`$${parseFloat(e.target.value).toFixed(2)}`)} />
             <button type='submit'>Submit</button>
         </form>
     );
