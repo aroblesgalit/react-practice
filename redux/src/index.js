4c481023c78afb1be1c32a64679043927ec50520
Alvin Galit <aroblesgalit@gmail.com>
2022-06-22 22:26:09 -0600

    Created actions for reference

--- redux/src/index.js
+++ redux/src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { createStore } from 'redux';
+import { bankingReducer } from './reducers/bankingReducers'
 
-const store = createStore();
+const store = createStore(bankingReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-
--- redux/src/reducers/bankingReducers.js
+++ redux/src/reducers/bankingReducers.js
@@ -1,3 +1,28 @@
 export const bankingReducer = (state, action) => {
     return state; // return the old state
 }
+
+
+// actions
+
+// deposit
+const deposit = {
+    type: 'DEPOSIT',
+    payload: 20
+  }
+  
+  // withdraw
+  const withdraw = {
+    type: 'WITHDRAW',
+    payload: 3
+  }
+  
+  // collect interest
+  const collectInterest = {
+    type: 'COLLECT_INTEREST'
+  }
+  
+  // delete account
+  const deleteAccount = {
+    type: 'DELETE_ACCOUNT'
+  }
