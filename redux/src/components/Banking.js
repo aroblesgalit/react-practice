3ecd92406114555f853cce08618c670b6b937adb
Alvin Galit <aroblesgalit@gmail.com>
2022-06-22 21:48:37 -0600

    Added form with input and buttons to banking component

--- redux/src/components/Banking.js
+++ redux/src/components/Banking.js
@@ -2,6 +2,13 @@ import React from 'react';
 
 export default function Banking() {
   return (
-    <div>Banking</div>
+    <div className='form-group'>
+        <input type='text' className='form-control' />
+        <button className='btn btn-success'>Deposit</button>
+        <button className='btn btn-primary'>Withdraw</button>
+        <button className='btn btn-warning'>Collect Interest</button>
+        <button className='btn btn-danger'>Delete Account</button>
+        <button className='btn btn-secondary'>Change Account Type</button>
+    </div>
   );
 }
