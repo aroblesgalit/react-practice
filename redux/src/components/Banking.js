984445dcd5741b67b9d4bbfa3a176041a26f1929
Alvin Galit <aroblesgalit@gmail.com>
2022-06-23 21:14:45 -0600

    Created state for amount in input field and boilerplates for each handle events for buttons

--- redux/src/components/Banking.js
+++ redux/src/components/Banking.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export default function Banking() {
+
+  const [amount, setAmount] = useState('');
+
+  function handleDeposit() {
+
+  }
+
+  function handleWithdraw() {
+    
+  }
+
+  function handleCollectInterest() {
+    
+  }
+
+  function handleDelete() {
+    
+  }
+
+  function handleAccountChange() {
+    
+  }
+
   return (
     <div className='form-group'>
-        <input type='text' className='form-control' />
-        <button className='btn btn-success'>Deposit</button>
-        <button className='btn btn-primary'>Withdraw</button>
-        <button className='btn btn-warning'>Collect Interest</button>
-        <button className='btn btn-danger'>Delete Account</button>
-        <button className='btn btn-secondary'>Change Account Type</button>
+        <input value={amount} onChange={(e) => setAmount(e.target.value)} type='text' className='form-control' />
+        <button onClick={handleDeposit} className='btn btn-success'>Deposit</button>
+        <button onClick={handleWithdraw} className='btn btn-primary'>Withdraw</button>
+        <button onClick={handleCollectInterest} className='btn btn-warning'>Collect Interest</button>
+        <button onClick={handleDelete} className='btn btn-danger'>Delete Account</button>
+        <button onClick={handleAccountChange} className='btn btn-secondary'>Change Account Type</button>
     </div>
   );
 }
