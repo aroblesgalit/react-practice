8f6eade897c7663115ff6e683888b2c9d7e57269
Alvin Galit <aroblesgalit@gmail.com>
2022-06-23 21:10:37 -0600

    Wrapped App in Provider

--- redux/src/index.js
+++ redux/src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { createStore } from 'redux';
-import { bankingReducer } from './reducers/bankingReducers'
+import { bankingReducer } from './reducers/bankingReducers';
+import { Provider } from 'react-redux';
 
 const store = createStore(bankingReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
