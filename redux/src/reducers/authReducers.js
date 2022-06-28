5a3f4eb327f14dbb326c48fe8ac163bb91fc950a
Alvin Galit <aroblesgalit@gmail.com>
2022-06-27 20:21:28 -0600

    Created reducer for auth

---
+++ redux/src/reducers/authReducers.js
@@ -0,0 +1,15 @@
+const initialState = {
+    isLoggedIn: false
+}
+
+export const authReducer = (state = initialState, action) => {
+    switch (action.type) {
+        case 'TOGGLE_AUTH':
+            return {
+                ...state,
+                isLoggedIn: !state.isLoggedIn
+            }
+        default: 
+            return state;
+    }
+}
