0275b208afc975e043201be479b2265eb9b7bace
Alvin Galit <aroblesgalit@gmail.com>
2022-06-27 20:15:21 -0600

    Used spread operator to return full state in reducers

--- redux/src/reducers/bankingReducers.js
+++ redux/src/reducers/bankingReducers.js
@@ -7,22 +7,27 @@ export const bankingReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'DEPOSIT':
             return {
+              ...state,
               balance: state.balance + action.payload
             }
         case 'WITHDRAW':
             return {
+              ...state,
               balance: state.balance - action.payload
             }
         case 'COLLECT_INTEREST':
             return {
+              ...state,
               balance: state.balance * 1.03
             }
         case 'DELETE_ACCOUNT':
             return {
+              ...state,
               balance: 0
             }
         case 'TOGGLE_ACCOUNT':
             return {
+              ...state,
               isSavingsAccount: !state.isSavingsAccount
             }
         default:
