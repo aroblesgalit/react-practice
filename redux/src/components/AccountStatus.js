f986e1f6e5d1f3d68c2da2eaa75f4cec67102868
Alvin Galit <aroblesgalit@gmail.com>
2022-06-27 20:14:05 -0600

    Rendered account type state in AccountStatus component

--- redux/src/components/AccountStatus.js
+++ redux/src/components/AccountStatus.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
 export default function AccountStatus() {
+
+  const { isSavingsAccount } = useSelector(state => state);
+
   return (
     <div>
-        <h1>Savings Account</h1>
+        <h1>{isSavingsAccount ? 'Savings' : 'Checking'} Account</h1>
     </div>
   );
 }
