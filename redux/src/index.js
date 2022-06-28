9623ca798b4d5374b69ff096d06fda4e0c741f0f
Alvin Galit <aroblesgalit@gmail.com>
2022-06-27 20:26:43 -0600

    Passed rootReducer into createStore

--- redux/src/index.js
+++ redux/src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
+import { rootReducer } from './reducers/combineReducer';
 
-const store = createStore(bankingReducer);
+const store = createStore(rootReducer);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
