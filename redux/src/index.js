93d982d14969b92a7f108d720de91f746dc9bd4a
Alvin Galit <aroblesgalit@gmail.com>
2022-06-27 20:25:17 -0600

    created combined reducers

--- redux/src/index.js
+++ redux/src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { createStore } from 'redux';
-import { bankingReducer } from './reducers/bankingReducers';
 import { Provider } from 'react-redux';
 
 const store = createStore(bankingReducer);
