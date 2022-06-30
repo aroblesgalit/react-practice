946a8ee7afa96685fe498099a644d4c01418df09
Alvin Galit <aroblesgalit@gmail.com>
2022-06-29 20:44:27 -0600

    Implemented store in index

--- redux_middleware_async_thunk/src/index.js
+++ redux_middleware_async_thunk/src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { createStore } from 'redux';
 import { counterReducer } from './reducer';
+import { Provider } from 'react-redux';
 
 const store = createStore(counterReducer);
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'));
