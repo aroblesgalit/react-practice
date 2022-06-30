0b61894a3e75d1d9db33e79a0400f6f4594257e5
Alvin Galit <aroblesgalit@gmail.com>
2022-06-29 20:43:29 -0600

    imported state into App and binded increment and decrement actions to buttons

--- redux_middleware_async_thunk/src/App.js
+++ redux_middleware_async_thunk/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment } from './action';
 
 export default function App() {
+
+  const count = useSelector(state => state);
+  const dispatch = useDispatch();
+
   return (
     <div>
-      <h1></h1>
-      <button>Decrement</button>
-      <button>Increment</button>
+      <h1>{count}</h1>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(increment())}>Increment</button>
     </div>
   );
 }
--- redux_middleware_async_thunk/src/index.js
+++ redux_middleware_async_thunk/src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { createStore } from 'redux';
+import { counterReducer } from './reducer';
+
+const store = createStore(counterReducer);
 
 ReactDOM.render(<App />, document.getElementById('root'));
